826bb39a5011674c501ac61b2a8cefb56ac8b4bf
unknown <rudacate02@hotmail.com>
2024-10-10 17:13:26 -0400

    Working on the local update

--- read_data_pglib.py
+++ read_data_pglib.py
@@ -1,6 +1,7 @@
 import matlab.engine as mat_eng
 import numpy as np
 import matlab
+from pypower.ext2int import ext2int
 
 def read_matpower_powergrid(dir_file):
     eng = mat_eng.start_matlab()
@@ -8,6 +9,7 @@ def read_matpower_powergrid(dir_file):
     eng.addpath(dir_file, nargout=0)
     # Load the MATPOWER case
     mpc = eng.pglib_opf_case5_pjm(nargout=1)
+    #mpc = ext2int(mpc)
     # Convert to internal indexing to get 'order' field
     base_mva = mpc["baseMVA"]
     
@@ -20,6 +22,12 @@ def read_matpower_powergrid(dir_file):
     gencost = np.array(mpc['gencost'])
     # Branch data
     branch = np.array(mpc['branch'])
+
+    #Convert to zero-indexed matrix
+    bus[:,0] -= 1
+    gen[:,0] -= 1
+    branch[:,0:2] -= 1
+
     return {
         "baseMVA": base_mva,
         #"areas": areas,
